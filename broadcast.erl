01e18e7b74c20d4079e8b848d127536774eecff7
Joaquin Arroyo <joaquin.arroyo100@gmail.com>
2021-07-13 19:12:37 -0300

    Atomic broadcast

--- broadcast.erl
+++ broadcast.erl
@@ -7,6 +7,7 @@
 -define(F, 0). %% number of nodes than can crash
 -define(TO, 200). %% TO value (milliseconds)
 
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Principal functions %%%
 
